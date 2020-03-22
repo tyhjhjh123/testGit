7aa368f74c54a26f496772bd5dc96c5158d2f1af
tyhjhjh123 <32112860+tyhjhjh123@users.noreply.github.com>
2020-03-22 18:49:09 +0800

    Add files via upload

---
+++ testC.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(){
+    cout<<"input:";
+    int a;
+    cin>>a;
+    cout<<"hello world!"<<endl;
+    system("pause");
+    return 0;
+}
